9e08f3ce98758a4e756be9d0f0b951ce312b6998
miikamenk <miikarito@gmail.com>
2026-03-17 13:24:51 +0200

    added firebase config for firestore and auth and downgraded to expo v54

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore, Firestore } from "firebase/firestore";
+import { getAuth, Auth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
+export const db: Firestore = getFirestore(app);
+export const auth: Auth = getAuth(app);
